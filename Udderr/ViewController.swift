b5c1a393345812d95c7abe273027e2c50b87b208
Nazym <Nazym23>
2021-07-20 19:15:44 +0600

    Initial Commit

---
+++ Udderr/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Udderr
+//
+//  Created by Nazym Mukashova on 20.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
